e285d234cc85687803faa6d92e1f5fd170cf84e5
Tushar <tushariitb2@gmail.com>
2015-03-20 03:42:35 +0530

    Adding pedestrians

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+<title>BTP II</title>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script>
 $(document).ready(function(){
@@ -45,12 +46,39 @@ body{
 	</div>
 
 <script type="text/javascript">
-function generateLeftBound{
-	
+function moveSteps(element, steps, direction){
+	// console.log(element);
 }
-function generateRightBound{
-	
+var lbArray = [];
+var rbArray = [];
+for(var i=0;i<20;i++){
+	lbArray.push(Math.floor(Math.random()*30));
 }
+for(var i=0;i<20;i++){
+	rbArray.push(Math.floor(Math.random()*30));
+}
+$.each(lbArray, function(index, value){
+	$('[data-row='+value+'][data-column="29"]').addClass('leftBound');
+});
+$.each(rbArray, function(index, value){
+	$('[data-row='+value+'][data-column="0"]').addClass('rightBound');
+});
+setInterval(function() {
+      $(".leftBound").each(function(index){
+      	$(this).removeClass('leftBound');
+      	var colNewL = parseInt($(this).data('column'))-1;
+      	var rowL = $(this).data('row');
+      	$('[data-row='+rowL+'][data-column="'+colNewL+'"]').addClass('leftBound');
+      });
+      $(".rightBound").each(function(index){
+      	moveSteps(this, 1);
+      	$(this).removeClass('rightBound');
+      	var colNewR = parseInt($(this).data('column'))+1;
+      	var rowR = $(this).data('row');
+      	$('[data-row='+rowR+'][data-column="'+colNewR+'"]').addClass('rightBound');
+      });
+}, 1000);
+
 </script>
 </body>
 </html>
