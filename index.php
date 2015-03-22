0b1baed4117304f1fad34c68890032a48c6f3206
Tushar <tushariitb2@gmail.com>
2015-03-23 03:22:33 +0530

    added functions to calculate same direction and opposite direction walkers in same lane

--- index.php
+++ index.php
@@ -52,6 +52,50 @@ function moveSteps(element, steps, direction, cls){
   	var rowR = $(element).data('row');
   	$('[data-row='+rowR+'][data-column="'+colNewR+'"]').addClass(cls);
 }
+function same_dir_same_lane_dist(element, cls){
+	var row = $(element).data('row');
+	var column = $(element).data('column');
+	var same_dir_walkers = [];
+	var dist = [];
+	$('.'+cls+'[data-row='+row+']').each(function(){
+		var col = $(this).data('column');
+		same_dir_walkers.push(col);
+	});
+	console.log(same_dir_walkers);
+	$.each(same_dir_walkers, function(index, value){
+		var val = parseInt(value) - 29;
+		dist.push(val);
+	});
+	console.log(dist);
+}
+function opp_dir_same_lane_dist(element, cls, ocls){
+	var row = $(element).data('row');
+	var column = $(element).data('column');
+	var opp_dir_walkers = [];
+	var dist = [];
+	$('.'+ocls+'[data-row='+row+']').each(function(){
+		var col = $(this).data('column');
+		opp_dir_walkers.push(col);
+	});
+	console.log(opp_dir_walkers);
+	$.each(opp_dir_walkers, function(index, value){
+		var val = parseInt(value) - 29;
+		dist.push(val);
+	});
+	console.log(dist);
+}
+function same_dir_top_lane_dist(element, cls){
+	console.log("3 success");
+}
+function opp_dir_top_lane_dist(element, cls, ocls){
+	console.log("4 success");
+}
+function same_dir_bottom_lane_dist(element, cls){
+	console.log("5 success");
+}
+function opp_dir_bottom_lane_dist(element, cls, ocls){
+	console.log("6 success");
+}
 var lbArray = [];
 var rbArray = [];
 for(var i=0;i<20;i++){
@@ -66,12 +110,24 @@ $.each(lbArray, function(index, value){
 $.each(rbArray, function(index, value){
 	$('[data-row='+value+'][data-column="0"]').addClass('rightBound');
 });
-setInterval(function() {
+setTimeout(function() {
       $(".leftBound").each(function(index){
+      	var sdsl = same_dir_same_lane_dist(this, 'leftBound');
+      	var odsl = opp_dir_same_lane_dist(this, 'leftBound', 'rightBound');
+      	// var sdtl = same_dir_top_lane_dist(this, 'leftBound');
+      	// var odtl = opp_dir_top_lane_dist(this, 'leftBound', 'rightBound');
+      	// var sdbl = same_dir_bottom_lane_dist(this, 'leftBound');
+      	// var odbl = opp_dir_bottom_lane_dist(this, 'leftBound', 'rightBound');
       	moveSteps(this, 1, -1, 'leftBound');
       });
       $(".rightBound").each(function(index){
-      	moveSteps(this, 1, 1, 'rightBound');
+      	// var sdsl = same_dir_same_lane_dist(this, 'rightBound');
+      	// var odsl = opp_dir_same_lane_dist(this, 'rightBound', 'leftBound');
+      	// var sdtl = same_dir_top_lane_dist(this, 'rightBound');
+      	// var odtl = opp_dir_top_lane_dist(this, 'rightBound', 'leftBound');
+      	// var sdbl = same_dir_bottom_lane_dist(this, 'rightBound');
+      	// var odbl = opp_dir_bottom_lane_dist(this, 'rightBound', 'leftBound');
+      	// moveSteps(this, 1, 1, 'rightBound');
       });
 }, 1000);
 
